8d5a18d62b1756e2cbebe551d716b2d1497b5b1d
Anthonyvillaaa <161419583+Anthonyvillaaa@users.noreply.github.com>
2025-01-30 10:23:02 -0800

    Update PascalTriangle.java

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -6,24 +6,24 @@ public class PascalTriangle {
 		Scanner scan = new Scanner(System.in);
 		int rows = scan.nextInt();
 
-		int[][] pascalTriangle = new int[rows][rows];
+		int[][] pascalTriangle = new int[rows][rows];			//Creating array
 		
 		for (int i = 0 ; i < rows ; i++){
 			for (int j = 0; j <= i; j++){
 				if(j == 0 || j == i){
-					pascalTriangle[i][j] = 1;
+					pascalTriangle[i][j] = 1;		//Creating border 1's
 				}
 				else{
-					pascalTriangle[i][j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j];
+					pascalTriangle[i][j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j];	//Using sum of previous entries above to calculate each element value
 				}
 			}
 		}
 
 		for (int i = 0 ; i < rows ; i++){
 			for (int j = 0; j <= i; j++){
-				System.out.print(pascalTriangle[i][j] + " ");
+				System.out.print(pascalTriangle[i][j] + " ");	//Printing Pascal Triangle
 			}
-			System.out.println();
+			System.out.println();	
 		}
 
 
